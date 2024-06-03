3fbd9004c379a72c4ae02662425d3ccf522c77ec
dhitozarfhan <dhitozarfhan@gmail.com>
2024-06-03 10:43:17 +0700

    manual exception

---
+++ Manualexceptiom/Manualexceptiom.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <exception>
+using namespace std;
+
+int main()
+{
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+
+    }
+    catch (int a) {
+        cout << "pengecualian akan dieksekusi" << endl;
+    }
+    catch (...) {
+        cout << "default pengecualian dieksekusi" << endl;
+    }
+    return 0;
+}
