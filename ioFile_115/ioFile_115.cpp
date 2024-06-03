f159a02b9b963200489bd42920affd984140f966
dhitozarfhan <dhitozarfhan@gmail.com>
2024-06-03 09:53:12 +0700

    unlimited loop untuk menulis

--- ioFile_115/ioFile_115.cpp
+++ ioFile_115/ioFile_115.cpp
@@ -8,6 +8,14 @@ int main() {
 
     ofstream outfile;
     outfile.open("contohfile.txt");
-}
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    while (true) {
+        cout << "_ ";
+        getline(cin, baris);
+        if (baris == "q") break;
+        
+    }
 
 
