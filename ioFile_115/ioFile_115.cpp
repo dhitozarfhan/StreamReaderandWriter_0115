e007290134aabc320c48cb55120713bcafdc2157
dhitozarfhan <dhitozarfhan@gmail.com>
2024-06-03 10:02:06 +0700

    membuka file dalam mode membaca

--- ioFile_115/ioFile_115.cpp
+++ ioFile_115/ioFile_115.cpp
@@ -18,4 +18,17 @@ int main() {
         
     }
 
+    outfile.close();
+
+    ifstream infile;
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file" << endl;
+        if (infile.is_open())
+        {
+            while (getline(infile, baris))
+            {
+                cout << baris << '\n';
+            }
+        }
 
